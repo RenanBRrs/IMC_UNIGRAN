4a785bb118dc195b8e68af1cc8c97ff16a74c116
RenanBRrs <47403260+RenanBRrs@users.noreply.github.com>
2020-03-27 19:31:55 -0300

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -3,12 +3,12 @@ altura=float(input('Digite sua altura! '))
 imc = peso / (altura ** 2)
 print (' O IMC  dessa pessoa e: {:.1f}'.format(imc))
 if imc < 18.5:
-    print('Voce esta abaixo do peso')
-if imc >= 18.5 and imc <25:
-    print('Peso normal')
-if imc >= 25 and imc <30:
-    print('Sobrepeso')
-if imc >= 30 and imc <39.9:
-    print('Obesidade')
-    if imc > 40:
-        print('Morbida')
+    print('ANOREXIA')
+elif 18.5 <= imc < 25:
+    print('NORMAL')
+elif 25 <= imc < 30:
+    print('SOBREPESO')
+elif 30 <= imc < 40:
+    print('OBESIDADE')
+elif imc >= 40:
+    print('MORBIDO')
